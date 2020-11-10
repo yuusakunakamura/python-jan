a42d248cc430f029469da94ed63fbf632d4c72f6
中村優作 <kickyuu5@gmail.com>
2020-11-10 21:44:48 +0900

    check

--- jisyo.py
+++ jisyo.py
@@ -1,5 +1,5 @@
 # 画像用ハッシュ
-items_imge = {
+items_imges = {
     "剣" : "http://paiza.jp/learning/images/sword.png",
     "盾" : "http://paiza.jp/learning/images/shield.png",
     "回復薬" : "http://paiza.jp/learning/images/potion.png",
@@ -12,5 +12,5 @@ item_cnt = int(input())
 # 標準入力にあるアイテムを出力する
 while item_cnt > 0:
   item = input()
-  print("<img src = '" + items_imge[item] + "'>")
+  print("<img src = '" + items_imges[item] + "'>")
   item_cnt = item_cnt - 1
